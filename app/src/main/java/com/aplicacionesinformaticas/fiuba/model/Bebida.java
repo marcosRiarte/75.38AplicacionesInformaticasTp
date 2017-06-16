d01fa8c198132a8370ed705c839c7f4244474095
marcosRiarte <mriarte@fi.uba.ar>
2017-06-16 04:56:47 -0300

    Bebibda.java
    Tiene un atributo con la condicion medica donde esta esa bebida prohibida (ej diabetico tiene prohida bebidas con azucar)

---
+++ app/src/main/java/com/aplicacionesinformaticas/fiuba/model/Bebida.java
@@ -0,0 +1,47 @@
+package com.aplicacionesinformaticas.fiuba.model;
+
+/*Clase que representa los bebidas de los que una orden
+ademas cuenta con un vector de condciones medicas las cuales indican para que condiciones medicas la bebida  esta prohibida
+ */
+
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Bebida {
+
+
+    private String nombre;
+    private float precio;
+    private ArrayList <CondicionMedica>  condicionesMedicasDondeEstaProhibida;
+
+    public Ingrediente (String nombre,float precioXunidad ) {
+        this.nombre = nombre;
+        this.precio = precioXunidad;
+    }
+
+    public float getPrecio () {
+        return this.precio;
+    }
+
+    public void setPrecio(float nuevoPrecio) {
+        this.precio = nuevoPrecio;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre (String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+
+    public ArrayList<CondicionMedica> getCondicionesMedicasDondeEstaProhibida() {
+        return condicionesMedicasDondeEstaProhibida;
+    }
+
+    public void agregarCondicionMedica (CondicionMedica condicionMedica) {
+        this.condicionesMedicasDondeEstaProhibida.add(condicionMedica);
+    }
+
+
+}
