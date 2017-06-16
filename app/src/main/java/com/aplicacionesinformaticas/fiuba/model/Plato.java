3d15592cede6278726df8cfa3d6ab0e111474acd
marcosRiarte <mriarte@fi.uba.ar>
2017-06-16 05:28:17 -0300

    Plato.java

---
+++ app/src/main/java/com/aplicacionesinformaticas/fiuba/model/Plato.java
@@ -0,0 +1,44 @@
+package com.aplicacionesinformaticas.fiuba.model;
+
+/*Calcula el precio todal del plato en base a los ingredientes que contiene 
+ */
+import com.aplicacionesinformaticas.fiuba.model.Ingrediente
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Plato {
+    private float precioBase;
+    private float precioTotal;
+    private ArrayList <Ingrediente> ingredientesDelPlato;
+
+   Plato(float precioBase){
+       this.precioBase = precioBase;
+       this.precioTotal = 0;
+   }
+    public getPrecioBase() {
+        return this.precioBase;
+    }
+    
+    public void setPrecioBase(float nuevoPrecioBase) {
+        this.precioBase = nuevoPrecioBase;
+    }
+
+    public void agregarIngrediente(Ingrediente nuevoIngrediente) {
+        this.ingredientesDelPlato.add(nuevoIngrediente);
+    }
+
+    public ArrayList<Ingrediente> getIngredientesDelPlato() {
+        return  this.ingredientesDelPlato;
+    }
+
+    public float getPrecioTotal() {
+        for (int i = 0 ;i < this.ingredientesDelPlato.size();i++) {
+            this.precioTotal = this.precioTotal + this.ingredientesDelPlato.get(i).getPrecio();
+        }
+        
+        this.precioTotal = this.precioTotal + this.precioBase;
+        return this.precioTotal;
+    }
+    
+    
+}
