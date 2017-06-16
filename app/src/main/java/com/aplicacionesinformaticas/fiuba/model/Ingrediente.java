bcad06992e7bddd27339dc008ee01ed6055ae756
marcosRiarte <mriarte@fi.uba.ar>
2017-06-16 04:44:22 -0300

    Clase ingrediente
    Contiene las condiciones medicas donde el ingrediente esta prohidido
    Su precio es por unidad de medida (kg,paquete,botella,etc)

---
+++ app/src/main/java/com/aplicacionesinformaticas/fiuba/model/Ingrediente.java
@@ -0,0 +1,47 @@
+package com.aplicacionesinformaticas.fiuba.model;
+
+/*Clase que representa los ingredientes de los que se compone un plato
+ademas cuenta con un vector de condciones medicas las cuales indican para que condiciones medicas el ingrediente esta prohibido
+ */
+
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Ingrediente {
+
+
+    private String nombre;
+    private float precio;
+    private ArrayList <CondicionMedica>  condicionesMedicasDondeEstaProhibido;
+
+    public Ingrediente (String nombre,float precioXunidadDeMedida ) {
+        this.nombre = nombre;
+        this.precio = precioXunidadDeMedida;
+    }
+    
+    public float getPrecio () {
+        return this.precio;
+    }
+    
+    public void setPrecio(float nuevoPrecio) {
+        this.precio = nuevoPrecio;
+    }
+    
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public void setNombre (String nuevoNombre) {
+        this.nombre = nuevoNombre;
+    }
+    
+    public ArrayList<CondicionMedica> getCondicionesMedicasDondeEstaProhibido() {
+        return condicionesMedicasDondeEstaProhibido;
+    }
+
+    public void agregarCondicionMedica (CondicionMedica condicionMedica) {
+        this.condicionesMedicasDondeEstaProhibido.add(condicionMedica);
+    }
+
+
+}
